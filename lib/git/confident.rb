c89d4078c3bd9e0f471af6a64395bc57a2fe47c9
Joe Yates <joe.g.yates@gmail.com>
2010-09-19 18:57:17 +0100

    Implemented the restore action

--- lib/git/confident.rb
+++ lib/git/confident.rb
@@ -13,14 +13,41 @@ module Git
       STRING = [ MAJOR, MINOR, TINY ].join( '.' )
     end
 
-    def initialize( path )
-      raise "Git repository not found at '#{ path }'" if ! File.directory?( "#{ path }/.git" )
-      @path = path.clone
+    def initialize( options )
+      @path      = options[ :path ].clone
+      @no_commit = options[ :no_commit ] ? true : false
+
+      raise "Git repository not found at '#{ @path }'" if ! File.directory?( "#{ @path }/.git" )
+
       super( { :working_directory => @path } )
+
+      case options[ :action ]
+      when :backup
+        backup
+      when :list
+        puts files
+      when :restore
+        restore
+      end     
     end
 
     def files
       ls_files.keys.sort
+    end
+
+    private
+
+    def backup
+      local_backup
+      return if @no_commit
+      commit
+      push
+    end
+
+    def restore
+      IO.popen( "rsync --no-perms --executability --files-from=- #{ @path }/ /", "w+" ) do | pipe |
+        files.each { | pathname | pipe.puts pathname }
+      end
     end
 
     def local_backup
